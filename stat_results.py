0bdbe8aac0dc3e00f5382e0b6bd77e49e5803565
Otman <otman.cherrati@gmail.com>
2024-06-23 05:45:12 -0400

    ubuntu to windows

--- stat_results.py
+++ stat_results.py
@@ -101,7 +101,7 @@ if None in sharps:
 df = pd.DataFrame(list(sharps.items()), columns=['groupe macro', 'sharpe'])
 df = df.sort_values(by='sharpe',ascending=False)
 df = df.round(decimals=3)
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots()
 
 # Hide axes
 ax.axis('tight')
